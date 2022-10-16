38746e964171ba58eda60d1653b9a2e2da358a78
Zelimkhan <ZYandarbiev@spb.beeline.ru>
2022-10-16 18:41:52 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:11
+EXPOSE 8000/tcp
+ARG JAR_FILE=target/health_check-0.0.1.jar
+WORKDIR /opt/app
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/dmp/beeline/ru/demo/controller/HealthCheck.java
@@ -0,0 +1,20 @@
+package dmp.beeline.ru.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health/")
+public class HealthCheck {
+    @GetMapping
+    public ResponseEntity<Map<String, String>> checkHealth() {
+        Map<String, String> map = new HashMap<>();
+        map.put("status", "ok");
+        return ResponseEntity.ok(map);
+    }
+}
